706f72f851a7e6f6838c35d25222297d6d7f4b63
James Lane <lanejames35@gmail.com>
2017-06-09 16:10:05 -0400

    testing with array of errors

--- src/script/chartist-error-bars.js
+++ src/script/chartist-error-bars.js
@@ -8,8 +8,8 @@
   
 	var defaultOptions = {
 		confidenceLimit: {
-			upper: null,
-			lower: null
+			upper: [],
+			lower: []
 		}
 	};
 
@@ -17,14 +17,14 @@
   Chartist.plugins.errorBars = function (options) {
 	    
 	options = Chartist.extend({}, defaultOptions, options);
-		
+	
 	function addErrorBar(data, upper, lower){
 		// Create a SVG line element that draws an error bar at the top of each bar
 		var errorLine = new Chartist.Svg('line', { x1: [data.x1], y1: [lower], x2: [data.x2], y2: [upper] }, 'ct-error');
 		// With data.element we get the current SVG element - <line> 
 		// We want to apply our new line to the parent element - <g>
 		data.element.parent().append(errorLine);
-	}  
+	}	
 
 	return function errorBars(chart){
 		if(chart instanceof Chartist.Line){
@@ -36,6 +36,7 @@
 					// Test this implementation with different scenarios
 					// Add a cap to each error bar
 					// Make margin of error variable
+					var it = makeIterator
 					var lcl = data.y2 + options.confidenceLimit.lower;
 					var	ucl = data.y2 - options.confidenceLimit.upper;
 					// Do not allow the error bar to drop below the axis
@@ -54,11 +55,16 @@
 					// Test this implementation with different scenarios
 					// Add a cap to each error bar
 					// Make margin of error variable
-					var ucl = Chartist.projectLength( data.axisY.axisLength, options.confidenceLimit.upper, data.axisY.bounds );
-					var lcl = Chartist.projectLength( data.axisY.axisLength, options.confidenceLimit.lower, data.axisY.bounds );
-					// Do not allow the error bar to drop below the axis
-					if (lcl < data.y1){
-						addErrorBar(data, ucl, lcl);
+					for(i=0; i < data.series.length; i++){
+						for(var k=0; k < options.confidenceLimit.upper.length; k++){
+							var ucl = data.chartRect.y1 - Chartist.projectLength( data.axisY.axisLength, options.confidenceLimit.upper, data.axisY.bounds );
+							var lcl = data.chartRect.y1 - Chartist.projectLength( data.axisY.axisLength, options.confidenceLimit.lower, data.axisY.bounds );
+							// Do not allow the error bar to drop below the axis
+							if (lcl <= data.y1){
+								addErrorBar(data, ucl, lcl);
+
+							}
+						}
 					}
 				}
 			});	
